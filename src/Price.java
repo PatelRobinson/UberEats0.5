e651b80692fb0a697d4ce63680544776ccc03900
deskybrown <joser1@umbc.edu>
2018-11-29 16:22:52 -0500

    Prices

--- src/Price.java
+++ src/Price.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Price
 {
-    public static void prices(){
+    public static double prices(){
 
         String chickMenu[][] = {{"soda", "lemonade", "sweet tea"},{"Chicken Sandwhich", "Chicken nuggets", "Chicken strips"},{"ice cream", "fruit bowl", "milkshake"}};
         double menuprices[][]={{1.29, 0.99, 1.50},{4.99, 3.99, 4.59},{2.99, 2.00, 1000.00}};
@@ -10,23 +10,29 @@ public class Price
         Scanner order = new Scanner(System.in);
         System.out.println("Please add items to your order, Enter 'F' to finish");
         String myorder = order.next();
+        String yourOrder = "";
 
         while (!myorder.equalsIgnoreCase("f")){
             for(int i = 0; i < 3; i++){
                 for(int j = 0; j < 3; j++){
                     if(chickMenu[i][j].equalsIgnoreCase(myorder))
                     {
+                        yourOrder = yourOrder + chickMenu[i][j];
                         prices = prices + menuprices[i][j];
                     }
+                    //test
                 }
             }
             System.out.print("Your order is now : $");
             System.out.println(prices);
             System.out.println("Please add anything else you would like to order. Enter 'F' to finish");
-            String myorder = order.next();
+            String twoOrder = order.next();
+            myorder = twoOrder;
 
 
         }
+        prices = prices + prices * 0.6;
+        return prices;
     }
 }
-}
+
