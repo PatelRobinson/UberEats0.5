c9727634bfbed4d96e9b23e0408653414f46a291
Parth Patel <P53@gmail.com>
2018-12-03 12:43:14 -0500

    goes to menuselect

--- src/ChickFilA.java
+++ src/ChickFilA.java
@@ -1,14 +1,16 @@
 import java.util.Scanner;
 class ChickFilA {
-    public static void menuChickFilA(){
+    public static void menuChickFilA() {
 
-        String chickMenu[][] = {{"soda", "lemonade", "sweet tea"},{"Chicken Sandwhich", "Chicken nuggets", "Chicken strips"},{"ice cream", "fruit bowl", "milkshake"}};
+        String chickMenu[][] = {{"soda", "lemonade", "sweet tea"}, {"Chicken Sandwhich", "Chicken nuggets", "Chicken strips"}, {"ice cream", "fruit bowl", "milkshake"}};
 
+        System.out.println("Welcome to the ChickFilA menu.");
         ResChoice.choice();
         Scanner menuList = new Scanner(System.in);
 
         int menuSelect = menuList.nextInt();
 
         MenuSelect.menu(menuSelect, chickMenu);
+
     }
 }
