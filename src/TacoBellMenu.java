e3b96dcdcb260e63b13dd32dd49b6826b00ed766
Parth Patel <P53@gmail.com>
2018-12-03 12:49:15 -0500

    Shows Taco Bell menu

--- src/TacoBellMenu.java
+++ src/TacoBellMenu.java
@@ -2,13 +2,15 @@ import java.util.Scanner;
 public class TacoBellMenu {
     public static void menuTacoBell() {
 
-        String tacoMenu[][] = {{"soda", "water", "milk"}, {"taco", "burrito", "enchillada"}, {"ice cream", "churro", "something"}};
+        String tacoMenu[][] = {{"soda", "water", "milk"}, {"taco", "burrito", "nachos"}, {"ice cream", "churro", "something"}};
 
+        System.out.println("Welcome to the Taco Bell menu.");
         ResChoice.choice();
         Scanner menuList = new Scanner(System.in);
 
         int menuSelect = menuList.nextInt();
 
         MenuSelect.menu(menuSelect, tacoMenu);
+
     }
 }
