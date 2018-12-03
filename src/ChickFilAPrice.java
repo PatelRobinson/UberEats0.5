7f519515d5fdd29e14ca59253e53e21f3759c8e0
Parth Patel <P53@gmail.com>
2018-12-03 12:50:59 -0500

    Shows ChickFilA menu

---
+++ src/ChickFilAPrice.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class ChickFilAPrice {
+    public static double prices() {
+
+        String chickMenu[][] = {{"soda", "lemonade", "sweet tea"}, {"Chicken Sandwhich", "Chicken nuggets", "Chicken strips"}, {"ice cream", "fruit bowl", "milkshake"}};
+        double menuprices[][] = {{1.29, 0.99, 1.50}, {4.99, 3.99, 4.59}, {2.99, 2.00, 3.99}};
+
+        double prices = 0.0;
+        Scanner order = new Scanner(System.in);
+        System.out.println("Please add items to your order, Enter 'F' to finish");
+        String myorder = order.nextLine();
+        String yourOrder = "";
+
+        while (!myorder.equalsIgnoreCase("f")) {
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    if (chickMenu[i][j].equalsIgnoreCase(myorder)) {
+                        yourOrder = yourOrder + chickMenu[i][j];
+                        prices += menuprices[i][j];
+
+                        System.out.print("Your order is now : $");
+                        System.out.println(menuprices[i][j]);
+                        System.out.println("Please add anything else you would like to order. Enter 'F' to finish");
+
+                    }
+                }
+            }
+            String twoOrder = order.nextLine();
+            myorder = twoOrder;
+        }
+        return prices;
+
+    }
+}
