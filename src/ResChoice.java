bae0a245693b44e28e4c51c4b7c3b5444bdc4262
Parth Patel <P53@gmail.com>
2018-12-04 15:54:52 -0500

    Shows the options for menu selection.

--- src/ResChoice.java
+++ src/ResChoice.java
@@ -2,9 +2,8 @@ public class ResChoice {
 
     public static void choice (){
 
-        System.out.println("Welcome to the taco bell menu.");
         System.out.println("Press 0 to display drinks");
-        System.out.println("Press 1 to display entres");
+        System.out.println("Press 1 to display entrees");
         System.out.println("Press 2 to display desserts");
         System.out.println("Press 3 to display full menu");
     }
