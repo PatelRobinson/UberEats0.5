59b8ecb612c45010e16c902d41cb5c1bd6cfc8ce
napar <naparth@gmail.com>
2018-12-04 17:49:09 -0500

    Shows the main menu

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,10 +1,12 @@
-import java.util.Scanner;
+//displays the main menu
 public class MainMenu {
-
+    //This is the the method that establishes the begining of the program by creating a list of resturants
+    //And taking in the one that the user wants to use.
     public static void menu (){
 
         String menu [][] = new String[6][2];
 
+        //Creates the array that the lists or resturants exists in
         menu[0][0] = "Welcome to ";
         menu[0][1] = "UberEats0.5!";
 
@@ -23,19 +25,12 @@ public class MainMenu {
         menu[5][0] = "4: ";
         menu[5][1] = "Surprise me!";
 
+        //Prints out each part of the resturant in the proper order.
         for (int row = 0; row < menu.length; row++){
             for(int column = 0; column <menu[row].length; column++ ){
                 System.out.print(menu[row][column]);
             }
             System.out.println("");
         }
-
-        System.out.println("Please choose a resturant: ");
-        Scanner scan = new Scanner(System.in);
-        int c = scan.nextInt();
-
-        Choice.choice(c);
-
-
     }
 }
