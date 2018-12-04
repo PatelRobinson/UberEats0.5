91306d366ba73108721b6872d7d280c72ea190a4
napar <naparth@gmail.com>
2018-12-04 17:50:20 -0500

    Shows the pizza hut menu

--- src/PizzaHutMenu.java
+++ src/PizzaHutMenu.java
@@ -1,18 +1,23 @@
+//displays the pizza hut menu
 import java.util.Scanner;
 
 public class PizzaHutMenu {
 
+    //This is the method for the menu of Pizza Hut
+    //It contains the menu and uses it as a references to call the menuselect method to fill out the order
     public static void PizzaHutMenu() {
 
         Scanner scan = new Scanner(System.in);
-        String[][] pizzamenu = {{"Soda", "Water", "Juice"},
-                                {"Cheese Pizza", "Pepperoni Pizza", "Chicken Wings"},
-                                {"Chocolate Brownies", "Chocolate Chip Cookies", "Chocolate Cake"}};
+
+        //Establishes the menu for pizza hut
+        ResMenus menu = new ResMenus();
+
+        //Displays the menu for the user and takes in a value that they select for the restaurant
         ResChoice.choice();
 
         int choices = scan.nextInt();
 
-        MenuSelect.menu(choices, pizzamenu);
+        MenuSelect.menu(choices, menu.Menu(0));
 
     }
 }
