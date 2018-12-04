5c14e05ff1fca693430eba75fd77ae6a8b1c7cd0
napar <naparth@gmail.com>
2018-12-04 17:47:17 -0500

    Displays ChickFilA menu

--- src/ChickFilA.java
+++ src/ChickFilA.java
@@ -1,18 +1,23 @@
+//displays the chickfila menu
 import java.util.Scanner;
 class ChickFilA {
+    //This is the method for the menu of Chick-fil-a
+    //It contains the menu and uses it as a references to call the menuselect method to fill out the order
     public static void menuChickFilA() {
 
-        String chickMenu[][] = {{"Soda", "Lemonade", "Sweet Tea"},
-                                {"Chicken Sandwich", "Chicken Nuggets", "Chicken Strips"},
-                                {"Ice Cream", "Fruit Bowl", "Milkshake"}};
+        //Establishes the string for the menu of Chick-fil-a
+        ResMenus menu = new ResMenus();
 
         System.out.println("Welcome to the ChickFilA menu.");
+
+        //Displays the menu for the user and takes in a value that they select for the resturant
         ResChoice.choice();
         Scanner menuList = new Scanner(System.in);
 
+
         int menuSelect = menuList.nextInt();
 
-        MenuSelect.menu(menuSelect, chickMenu);
+        MenuSelect.menu(menuSelect, menu.Menu());
 
     }
 }
