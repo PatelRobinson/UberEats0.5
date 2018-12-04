e72e3465d97a0b9381232d3be9aad640bc283d7f
napar <naparth@gmail.com>
2018-12-04 17:51:36 -0500

    Provide the choice for which part of the menu to display

--- src/ResChoice.java
+++ src/ResChoice.java
@@ -1,10 +1,12 @@
-public class ResChoice {
-
+//Extention of the MainMenu class
+public class ResChoice extends MainMenu {
+    //Prints which part of the menu they want to use
     public static void choice (){
 
-        System.out.println("Press 0 to display drinks");
-        System.out.println("Press 1 to display entrees");
-        System.out.println("Press 2 to display desserts");
+        System.out.println("Press 0 if you ONLY want drinks");
+        System.out.println("Press 1 if you ONLY want entrees");
+        System.out.println("Press 2 if you ONLY want desserts");
         System.out.println("Press 3 to display full menu");
+
     }
 }
