73c04e8190c17efcff049f8820c0a0ba836d4687
napar <naparth@gmail.com>
2018-12-04 16:32:17 -0500

    Displays the correct part of the menu based on the choice

--- src/MenuSelect.java
+++ src/MenuSelect.java
@@ -31,10 +31,10 @@ public class MenuSelect {
                 for (int i = 0; i < 3; i++) {
                     for (int j = 0; j < 3; j++) {
 
-                        System.out.print(y[i][j]);
+                        System.out.print(y[j][i]);
                         System.out.print("    ");
                     }
-
+                    System.out.println();
                 }
             }
 
