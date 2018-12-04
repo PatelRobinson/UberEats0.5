d7e870e9e7298a5c14fe22b66547695fba3878b1
Parth Patel <P53@gmail.com>
2018-12-04 15:35:03 -0500

    Calls the menu for the corrosponding menu selected and then gives the total price.

--- src/Choice.java
+++ src/Choice.java
@@ -4,23 +4,23 @@ public class Choice {
     public static void choice (int x) {
 
         Scanner scan = new Scanner(System.in);
-        for (int num = 0; num < 4; num++) {
+        for (int num = 0; num < 5; num++) {
 
             switch (x) {
 
                 case 1:
                     TacoBellMenu.menuTacoBell();
-                    TacoBellPrice.price();
+                    System.out.println("Your total is: " + TacoBellPrice.price());
                     num += 5;
                     break;
                 case 2:
                     ChickFilA.menuChickFilA();
-                    PizzaHutPrice.price();
+                    System.out.println("Your total is: " + ChickFilAPrice.price());
                     num += 5;
                     break;
                 case 3:
                     PizzaHutMenu.PizzaHutMenu();
-                    ChickFilAPrice.price();
+                    System.out.println("Your total is: " + PizzaHutPrice.price());
                     num += 5;
                     break;
                 case 4:
