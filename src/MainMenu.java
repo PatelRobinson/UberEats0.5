741940f8529594905faa73a35507ab55c0e6d3e8
napar <naparth@gmail.com>
2018-12-04 18:00:53 -0500

    Shows the main menu

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -23,7 +23,7 @@ public class MainMenu {
         menu[4][1] = "Pizza Hut";
 
         menu[5][0] = "4: ";
-        menu[5][1] = "Surprise me!";
+        menu[5][1] = "Surprise me!(Special deal: Total is $5.00)";
 
         //Prints out each part of the resturant in the proper order.
         for (int row = 0; row < menu.length; row++){
