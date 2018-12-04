4c356a40ef818ae4ba07f9a881b173c44cb5f0c4
Parth Patel <P53@gmail.com>
2018-12-04 15:56:27 -0500

    Shows the section of the menu selected

--- src/MenuSelect.java
+++ src/MenuSelect.java
@@ -1,40 +1,42 @@
 public class MenuSelect {
 
     public static void menu(int x, String [][]y){
-        if (x == 0) {
-            System.out.println("DRINKS");
-            System.out.println("------");
 
-            for (int i = 0; i < y[x].length; i++) {
+            if (x == 0) {
+                System.out.println("DRINKS");
+                System.out.println("------");
 
-                System.out.println(y[x][i]);
-            }
-        } else if (x == 1) {
-            System.out.println("ENTRES");
-            System.out.println("------");
+                for (int i = 0; i < y[x].length; i++) {
 
-            for (int i = 0; i < y[x].length; i++) {
-
-                System.out.println(y[x][i]);
-            }
-        } else if (x == 2) {
-            System.out.println("DESSERT");
-            System.out.println("-------");
-
-            for (int i = 0; i < y[x].length; i++) {
-
-                System.out.println(y[x][i]);
-            }
-        } else if (x == 3) {
-            System.out.println("DRINKS | ENTRES | DESSERTS");
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-
-                    System.out.print(y[j][i]);
-                    System.out.print("    ");
+                    System.out.println(y[x][i]);
                 }
-                System.out.println();
+            } else if (x == 1) {
+                System.out.println("ENTREES");
+                System.out.println("------");
+
+                for (int i = 0; i < y[x].length; i++) {
+
+                    System.out.println(y[x][i]);
+                }
+            } else if (x == 2) {
+                System.out.println("DESSERT");
+                System.out.println("-------");
+
+                for (int i = 0; i < y[x].length; i++) {
+
+                    System.out.println(y[x][i]);
+                }
+            } else if (x == 3) {
+                System.out.println("DRINKS | ENTREES | DESSERTS");
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+
+                        System.out.print(y[i][j]);
+                        System.out.print("    ");
+                    }
+
+                }
             }
-        }
+
     }
 }
